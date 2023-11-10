c8525a353ee307dd33f02554364fb4a6bba80642
Andrea Del Corto <andrea.delcorto@gmail.com>
2023-11-10 23:42:59 +0100

    fix box animations

--- FormVsColor/Assets/Scripts/Box.cs
+++ FormVsColor/Assets/Scripts/Box.cs
@@ -6,34 +6,28 @@ using UnityEngine.UI;
 public class Box : MonoBehaviour {
     [SerializeField] private int currentStatus = 0;
     [SerializeField] private BoxStatus[] allowedStatuses;
-    [SerializeField] Sprite empty, fromBlackCross, formWhiteCircle, colorWhiteCross, colorBlackCircle;
     [SerializeField] Animator animator;
-    [SerializeField] private SpriteRenderer renderer;
     protected int row, col;
+
+    private Dictionary<(BoxStatus CurrentState, BoxStatus NextState), string> transitionAnimations = new() {
+        { (BoxStatus.EMPTY, BoxStatus.FORM_BLACK_CROSS), "form_black_cross_idle" },
+        { (BoxStatus.EMPTY, BoxStatus.FORM_WHITE_CIRCLE), "form_white_circle_idle" },
+        { (BoxStatus.EMPTY, BoxStatus.COLOR_BLACK_CIRCLE), "color_black_circle_idle" },
+        { (BoxStatus.EMPTY, BoxStatus.COLOR_WHITE_CROSS), "color_white_cross_idle" },
+        { (BoxStatus.FORM_BLACK_CROSS, BoxStatus.FORM_WHITE_CIRCLE), "form_flip_from_black_cross_to_white_circle" },
+        { (BoxStatus.FORM_WHITE_CIRCLE, BoxStatus.FORM_BLACK_CROSS), "form_flip_from_white_circle_to_black_cross" },
+        { (BoxStatus.COLOR_BLACK_CIRCLE, BoxStatus.COLOR_WHITE_CROSS), "color_flip_from_black_circle_to_white_cross" },
+        { (BoxStatus.COLOR_WHITE_CROSS, BoxStatus.COLOR_BLACK_CIRCLE), "color_flip_from_white_cross_to_black_circle" },
+    };
+
     public delegate void BoxClickedEventHandler(int row, int col);
+
     public event BoxClickedEventHandler OnBoxClicked;
 
-    public virtual void OnClick()
-    {
+    public virtual void OnClick() {
         OnBoxClicked?.Invoke(row, col);
     }
-    
-    private Sprite GetSpriteFromStatus(BoxStatus status) {
-        switch (status) {
-            case BoxStatus.EMPTY:
-                return empty;
-            case BoxStatus.FORM_BLACK_CROSS:
-                return fromBlackCross;
-            case BoxStatus.FORM_WHITE_CIRCLE:
-                return formWhiteCircle;
-            case BoxStatus.COLOR_WHITE_CROSS:
-                return colorWhiteCross;
-            case BoxStatus.COLOR_BLACK_CIRCLE:
-                return colorBlackCircle;
-            default:
-                throw new ApplicationException("Invalid BoxStatus");
-        }
-    }    
+
 
     public void Initialize(int row, int col) {
         this.row = row;
@@ -49,18 +43,18 @@ public class Box : MonoBehaviour {
         if (Array.IndexOf(allowedStatuses, nextStatus) == -1) {
             throw new ArgumentException("nextStatus is not in allowedStatuses");
         }
-        if (nextStatus != GetStatus()) {
-            currentStatus = Array.IndexOf(allowedStatuses, nextStatus);
-            HandleTransactionAnimation(GetStatus(), nextStatus);
-        }
+
+        if (GetStatus() == nextStatus) return;
+        animator.Play(GetAnimationName(GetStatus(), nextStatus));
+        currentStatus = Array.IndexOf(allowedStatuses, nextStatus);
     }
-    
+
     private string GetIdleAnimationName(BoxStatus status) {
         switch (status) {
             case BoxStatus.EMPTY:
                 return "empty_idle";
             case BoxStatus.FORM_BLACK_CROSS:
-                return "form_white_circle_idle";
+                return "form_black_cross_idle";
             case BoxStatus.FORM_WHITE_CIRCLE:
                 return "form_white_circle_idle";
             case BoxStatus.COLOR_WHITE_CROSS:
@@ -68,13 +62,13 @@ public class Box : MonoBehaviour {
             case BoxStatus.COLOR_BLACK_CIRCLE:
                 return "color_black_circle_idle";
             default:
-                throw new ApplicationException("Invalid BoxStatus");
+                throw new ApplicationException("BoxStatus does not have an idle animation: " + status);
         }
     }
-    
-    private void HandleTransactionAnimation(BoxStatus from, BoxStatus to) {
-        animator.Play(GetIdleAnimationName(to));
-        renderer.sprite = formWhiteCircle;
+
+    private string GetAnimationName(BoxStatus from, BoxStatus to) {
+        transitionAnimations.TryGetValue((from, to), out string animationName);
+        return animationName == null ? GetIdleAnimationName(to) : animationName;
     }
 
     public BoxStatus NextStatus() {
@@ -87,21 +81,20 @@ public class Box : MonoBehaviour {
     public void SetEmpty() {
         SetStatus(BoxStatus.EMPTY);
     }
-    
+
     public void SetFormBlackCross() {
         SetStatus(BoxStatus.FORM_BLACK_CROSS);
     }
-    
+
     public void SetFormWhiteCircle() {
         SetStatus(BoxStatus.FORM_WHITE_CIRCLE);
     }
-    
+
     public void SetColorWhiteCross() {
         SetStatus(BoxStatus.COLOR_WHITE_CROSS);
     }
-    
+
     public void SetColorBlackCircle() {
         SetStatus(BoxStatus.COLOR_BLACK_CIRCLE);
-    }        
-
+    }
 }
